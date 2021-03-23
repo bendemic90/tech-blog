956f9e95fc833d640334a8891d0b05d99844255b
Ben T <ben.trevethan@gmail.com>
2021-03-23 18:24:33 +1030

    trying to get profile rendering to work, updated handlebars templates

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const myProfile = require('./myProfile.js')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile', myProfile)
 
 module.exports = router;
