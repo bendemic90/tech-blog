d9b222e4f1acc7b31314983a6a215af4a44fb373
Ben T <ben.trevethan@gmail.com>
2021-03-26 13:14:16 +1030

    routing for Replies, fixed up JS script linked to handlebars

--- controllers/api/replyRoutes.js
+++ controllers/api/replyRoutes.js
@@ -39,3 +39,5 @@ router.post('/', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 })
+
+module.exports = router;
--- public/js/reply.js
+++ public/js/reply.js
@@ -2,10 +2,7 @@ const newReply = async (event) => {
     event.preventDefault();
 
     const reply_text = document.querySelector('#reply-body').value.trim();
-
-    const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+    const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
     if (reply_text) {
         const response = await fetch('/api/replies', {
