7954ba05c733a70d6965a026a4a63abcd9108a9c
Ben T <ben.trevethan@gmail.com>
2021-03-29 11:38:12 +1030

    heroku jawsdb test

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'dc0d3',
